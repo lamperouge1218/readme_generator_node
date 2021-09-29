1fd2c12285024836a1b91c01ae4ced14497a73f6
lamperouge <philbohn1791@gmail.com>
2021-09-29 16:33:38 -0400

    added the generateMarkdown.js file to the main index

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const utils = require("utils");
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
-        message: "What is the title of your project?" ,
+        message: "What is the title of your project?",
         name: "title"
     },
     {
         type: "input",
-        message: "Describe your project." ,
+        message: "Describe your project.",
         name: "description"
     },
     {
         type: "input",
-        message: "How would one install your project?" ,
+        message: "How would one install your project?",
         name: "installation"
     },
     {
         type: "input",
-        message: "How would one use your project?" ,
+        message: "How would one use your project?",
         name: "usage"
     },
     {
         type: "input",
-        message: "What license does your project use?" ,
+        message: "What license does your project use?",
         name: "license"
     },
     {
         type: "input",
-        message: "How would one contribute to your project?" ,
+        message: "How would one contribute to your project?",
         name: "contributing"
     },
     {
         type: "input",
-        message: "How would one test your project?" ,
+        message: "How would one test your project?",
         name: "tests"
     },
     {
         type: "input",
-        message: "How would one reach you for further questions about your project?" ,
+        message: "How would one reach you for further questions about your project?",
         name: "questions"
     },
 ];
 
-inquirer.prompt(questions).then((responses) => console.log(responses))
+inquirer
+    .prompt(questions)
+    .then((responses) =>
+        console.log(responses))
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
 
