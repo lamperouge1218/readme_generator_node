60fe4a501fd32dbd71c23f1fb26b060d22410e1e
lamperouge <philbohn1791@gmail.com>
2021-09-29 17:13:22 -0400

    got the generateMarkdown.js hooked up properly to the page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const utils = require("./utils");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -50,10 +50,10 @@ const questions = [
 inquirer
     .prompt(questions)
     .then((responses) =>
-        console.log(responses));
+        console.log(responses))
+
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
-
 // // TODO: Create a function to initialize app
 // function init() {}
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+// function renderLicenseBadge(license) { }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { }
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) { }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { }
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -16,7 +16,7 @@ function generateMarkdown(data) {
 
   ## ${data.description}
 
-  ## ${"Table of Contents" \n "-- [Usage](#usage)"\n "-- [Credits](#credits)" \n "-- [License](#license)"}
+  ## ${"Table of Contents"}\n${"-- [Usage](#usage)"}\n${"-- [Credits](#credits)"}\n${"-- [License](#license)"}
 
 `;
 }
