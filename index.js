409eb67dffed89203d09c7060ab588e9d556a4ec
lamperouge <philbohn1791@gmail.com>
2021-09-29 16:08:31 -0400

    got inquirer installed and started filling out questions to ask the user

--- index.js
+++ index.js
@@ -2,7 +2,48 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?" ,
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Describe your project." ,
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "How would one install your project?" ,
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "How would one use your project?" ,
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What license does your project use?" ,
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "How would one contribute to your project?" ,
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "How would one test your project?" ,
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "How would one reach you for further questions about your project?" ,
+        name: "questions"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
